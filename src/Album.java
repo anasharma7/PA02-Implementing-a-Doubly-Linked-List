a763942d431350553f0cfe1ca4330a878441fed8
anasharma7 <aanasharma15@gmail.com>
2023-04-05 02:44:46 -0400

    First Commit - Adding Album.java

---
+++ src/Album.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class Album implements Comparable<Album> {
+    private int id;
+    private String[] artists;
+    private String title;
+    private int numSongs;
+
+    public Album(int id, String[] artists, String title, int numSongs) {
+        this.id = id;
+        this.artists = artists;
+        this.title = title;
+        this.numSongs = numSongs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String[] getArtists() {
+        return artists;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumSongs() {
+        return numSongs;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setArtists(String[] artists) {
+        this.artists = artists;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setNumSongs(int numSongs) {
+        this.numSongs = numSongs;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %d -- %s", id, numSongs, Arrays.toString(artists));
+    }
+
+    @Override
+    public int compareTo(Album otherAlbum) {
+        return Integer.compare(this.numSongs, otherAlbum.getNumSongs());
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create some albums
+        Album album1 = new Album(1, new String[] {"Artist1", "Artist2"}, "Album Title 1", 10);
+        Album album2 = new Album(2, new String[] {"Artist3", "Artist4"}, "Album Title 2", 8);
+        Album album3 = new Album(3, new String[] {"Artist5", "Artist6"}, "Album Title 3", 12);
+
+        // Create a linked list of albums
+        LinkedList<Album> albumList = new LinkedList<>();
+        albumList.add(album1);
+        albumList.add(album2);
+        albumList.add(album3);
+
+        // Print the unsorted album list
+        System.out.println("Unsorted Album List:");
+        for (Album album : albumList) {
+            System.out.println(album.toString());
+        }
+
+        // Sort the album list and print it again
+        Collections.sort(albumList);
+        System.out.println("\nSorted Album List:");
+        for (Album album : albumList) {
+            System.out.println(album.toString());
+        }
+    }
+}
